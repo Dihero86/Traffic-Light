b4fe9658b74c4061a70fd2e4ffc49807e47aa234
Dihero86 <diegohrodriguez86@gmail.com>
2023-03-06 09:25:49 +0000

    feat: second Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,37 +6,34 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-	const colores={
-		red:"",
-		yellow:"",
-		green:"",
-		purple:"",
+	const colores = {
+		red: "",
+		yellow: "",
+		green: "",
+		purple: "",
 	}
 
-	const [color,setColor] = useState(colores);
-	const [add,setAdd] = useState("");
-	
-	useEffect(()=>{
-		setColor({...colores, green:"selected"})
-	},[])
-	
+	const [color, setColor] = useState(colores);
+	const [add, setAdd] = useState("");
+
+	useEffect(() => {
+		setColor({ ...colores, green: "selected" })
+	}, [])
 
 	return (
-		
-			<div className="trafficlight">
-				<div className="trafficlighttop" ></div>
-				<div className="trafficlightbody" >
-					<div className={"redlight " +color.red} onClick={()=>setColor({...colores, red:"selected"})}></div>
-					<div className={"yellowlight " +color.yellow} onClick={()=>setColor({...colores, yellow:"selected"})}> </div>
-					<div className={"greenlight " +color.green} onClick={()=>setColor({...colores, green:"selected"})}></div>
-					<div className={add + color.purple} onClick={()=>setColor({...colores, purple:"selected"})}></div>
-				</div>
-				<div className="addButton">
-				<button onClick={()=>setAdd("purplelight ")} >Agregar Luz</button>
-				</div>
+		<div className="trafficlight">
+			<div className="trafficlighttop" ></div>
+			<div className="trafficlightbody" >
+				<div className={"redlight " + color.red} onClick={() => setColor({ ...colores, red: "selected" })}></div>
+				<div className={"yellowlight " + color.yellow} onClick={() => setColor({ ...colores, yellow: "selected" })}> </div>
+				<div className={"greenlight " + color.green} onClick={() => setColor({ ...colores, green: "selected" })}></div>
+				<div className={add + color.purple} onClick={() => setColor({ ...colores, purple: "selected" })}></div>
 			</div>
-		
-		
+			<div className="addButton">
+				<button onClick={() => setAdd("purplelight ")} >Agregar Luz</button>
+			</div>
+		</div>
+
 	);
 };
 
